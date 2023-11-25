b9a4b4bcfcd6c9668cc56f019143bfd099e8668e
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-11-26 00:41:27 +0900

    repeller 회전속도 조절
    - setSpeed() 메서드를 추가하여 마우스 x 좌표에 따라 repeller 회전 속도를 조절 할 수 있음

--- repeller.js
+++ repeller.js
@@ -4,8 +4,13 @@ class Repeller {
     this.power = 300;
     this.angle = 0;
   }
+
+  setSpeed(value){
+    this.speed = map(value,0,width,-0.05,0.05);
+  }
+
    move(){
-    this.angle+= 0.05;
+    this.angle+= this.speed;
     this.position.x = width/2 + 150 * cos(this.angle);
     this.position.y = height/2 + 150* sin(this.angle);
    }
