2cef1e8b1a170436469f1aebe14654134c94e2ea
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-11-26 12:05:31 +0900

    particle 배치
    - particle 이 화면에 랜덤으로 배치될 수 있게 함

--- particle.js
+++ particle.js
@@ -1,35 +1,30 @@
 class Particle {
-  constructor(x, y) {
-    this.position = createVector(x, y);
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
-    this.acceleration = createVector(0, 0);
-    this.lifespan = 255.0;
+  constructor() {
+    this.pos = createVector(random(0, width), random(0, height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.w = 4;
   }
-
-  run() {
-    this.update();
-    this.show();
+  
+  addForce(aForce) {
+    this.acc.add(aForce);
   }
-
-  applyForce(f) {
-    this.acceleration.add(f);
-  }
-
+  
   update() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.lifespan -= 2;
-    this.acceleration.mult(0);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    
+    this.acc = createVector(0, 0);
+    this.vel.mult(0.9);
+    
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.y > height) this.pos.y = 0;
   }
-
+  
   show() {
-    noStroke();
-    fill(50, this.lifespan);
-    circle(this.position.x, this.position.y, 6);
-  }
-
-
-  isDead() {
-    return this.lifespan < 0.0;
+    fill(0);
+    circle(this.pos.x, this.pos.y, this.w);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,37 +1,66 @@
-let emitter;
-
-let repeller;
-
-let att;
+let particles = [];
+let rPs = [];
 
 function setup() {
-  createCanvas(800, 600);
-  emitter1 = new Emitter(width / 2, 100);
-  emitter2 = new Emitter(width / 2, height-100);
-  repeller = new Repeller(width / 2, height/2);
-  att1 = new Attractor(235, height/2);
-  att2 = new Attractor(565, height/2);
+  createCanvas(400, 400);
+  noStroke();
+  rPs = createParticles(100); 
 }
 
+
 function draw() {
-  background(255);
-  repeller.move();
-  emitter1.addParticle();
-  emitter1.addParticle();
-  emitter2.addParticle();
-  emitter2.addParticle();
-  let gravity1 = createVector(0, 0.005);
-  emitter1.applyForce(gravity1);
-  emitter1.applyRepeller(repeller);
-  emitter1.applyAttractor(att1);
-  emitter1.applyAttractor(att2);
-  emitter1.run();
-  let gravity2 = createVector(0, -0.005);
-  emitter2.applyForce(gravity2);
-  emitter2.applyRepeller(repeller);
-  emitter2.applyAttractor(att1);
-  emitter2.applyAttractor(att2);
-  emitter2.run();
-  repeller.show();
+  background(225);
+  
+  for (let i=0; i<particles.length; i++) {
+    let p = particles[i];
+    p.update();
+    p.show();
+  }
 }
+
+
+function createParticles(number) {
+  for (let i=0; i<number; i++) {
+    let p = new Particle();
+    particles.push(p);
+  }
+}
+
+
+// let emitter;
+
+// let repeller;
+
+// let att;
+
+// function setup() {
+//   createCanvas(800, 600);
+//   emitter1 = new Emitter(width / 2, 100);
+//   emitter2 = new Emitter(width / 2, height-100);
+//   repeller = new Repeller(width / 2, height/2);
+//   att1 = new Attractor(235, height/2);
+//   att2 = new Attractor(565, height/2);
+// }
+
+// function draw() {
+//   background(255);
+//   repeller.move();
+//   emitter1.addParticle();
+//   emitter1.addParticle();
+//   emitter2.addParticle();
+//   emitter2.addParticle();
+//   let gravity1 = createVector(0, 0.005);
+//   emitter1.applyForce(gravity1);
+//   emitter1.applyRepeller(repeller);
+//   emitter1.applyAttractor(att1);
+//   emitter1.applyAttractor(att2);
+//   emitter1.run();
+//   let gravity2 = createVector(0, -0.005);
+//   emitter2.applyForce(gravity2);
+//   emitter2.applyRepeller(repeller);
+//   emitter2.applyAttractor(att1);
+//   emitter2.applyAttractor(att2);
+//   emitter2.run();
+//   repeller.show();
+// }
 
