07aa1d602fd2a33f21c2b7fd98cdb997b9d3b30f
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-12-03 18:34:21 +0900

    input button 추가

--- sketch.js
+++ sketch.js
@@ -1,49 +1,47 @@
+let input, button, greeting;
+
 let emitter;
+
 let repeller;
-
-let gui;
-
-let params = {
-  rotatespeed: 0.01,
-  rotatespeedMin: -0.05,
-  rotatespeedMax: 0.05,
-  rotatespeedStep: 0.005,
-  particlevalue: 3,
-  particlevalueMin: 0,
-  particlevalueMax: 7,
-  particlevalueStep: 0.5,
-  repellerpower: 400,
-  repellerpowerMin: 0,
-  repellerpowerMax: 1000,
-  repellerpowerStep: 10
-};
 
 function setup() {
   createCanvas(500, 500);
-
   emitter = new Emitter(width / 2, height / 2);
   repeller = new Repeller(width / 2, height / 2);
 
-  gui = createGui("test slider");
+  input = createInput();
+  input.position(20, 65);
 
-  gui.addObject(params);
-  gui.setPosition(510, 10);
+  button = createButton("submit");
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement("h2", "choose an emoji");
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
 }
 
 function draw() {
   background(255);
-  repeller.setPower(params.repellerpower);
-  repeller.setSpeed(params.rotatespeed);
   repeller.move();
-
-  for (let i = 0; i < params.particlevalue; i++) {
-    emitter.addParticle();
-  }
-
-  let gravity = createVector(0, 0.1);
+  emitter.addParticle();
+  emitter.addParticle();
+  emitter.addParticle();
+  let gravity = createVector(0, 0.05);
   emitter.applyForce(gravity);
   emitter.applyRepeller(repeller);
   emitter.run();
 
   repeller.show();
+}
+
+function greet() {
+  const name = input.value();
+  greeting.html("WOW " + name + " !");
+
+  for (let i = 0; i < 20; i++) {
+    emitter.addParticle(name);
+  }
 }
