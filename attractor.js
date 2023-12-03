5b082fa3d7fe9def3a8eba2572fcd135d3323747
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-12-03 19:22:31 +0900

    가장 마음에 드는 표현물 불러오기

--- attractor.js
+++ attractor.js
@@ -1,28 +1,29 @@
 class Attractor {
-    constructor() {
-      this.position = createVector(width / 2, height / 2);
-      this.power = 8000;
-      this.angle = 0;
-    }
-  
-    move() {
-      this.angle += 0.02;
-      this.position.x = width / 2 - 250 * cos(this.angle);
-      this.position.y = height / 2 - 250 * sin(this.angle);
-    }
-  
-    pull(particle) {
-      let force = p5.Vector.sub(this.position, particle.position);
-      let distance = force.mag();
-      distance = constrain(distance, 5, 50);
-      let strength = this.power / (distance * distance);
-      force.setMag(strength);
-      return force;
-    }
-  
-    show() {
-      fill(0);
-      circle(this.position.x, this.position.y, 32);
-    }
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.power = 500;
+    this.angle = 0;
   }
   
+  move() {
+    this.angle+= 0.07;
+    this.position.x = repeller.position.x - 150 * cos(this.angle);
+    this.position.y = repeller.position.y - 150 * sin(this.angle);
+  }
+  
+  show() {
+    stroke(0);
+    strokeWeight(2);
+    fill(200, 130, 50);
+    circle(this.position.x, this.position.y, 32);
+  }
+
+  pull(particle) {
+    let force = p5.Vector.sub(this.position, particle.position);
+    let distance = force.mag();
+    distance = constrain(distance, 5, 50);
+    let strength = this.power / (distance * distance);
+    force.setMag(strength);
+    return force;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,49 +1,38 @@
 let emitter;
+
 let repeller;
 
-let gui;
-
-let params = {
-  rotatespeed: 0.01,
-  rotatespeedMin: -0.05,
-  rotatespeedMax: 0.05,
-  rotatespeedStep: 0.005,
-  particlevalue: 3,
-  particlevalueMin: 0,
-  particlevalueMax: 7,
-  particlevalueStep: 0.5,
-  repellerpower: 400,
-  repellerpowerMin: 0,
-  repellerpowerMax: 1000,
-  repellerpowerStep: 10
-};
+let att;
 
 function setup() {
-  createCanvas(500, 500);
-
-  emitter = new Emitter(width / 2, height / 2);
-  repeller = new Repeller(width / 2, height / 2);
-
-  gui = createGui("test slider");
-
-  gui.addObject(params);
-  gui.setPosition(510, 10);
+  createCanvas(800, 600);
+  emitter1 = new Emitter(width / 2, 100);
+  emitter2 = new Emitter(width / 2, height-100);
+  repeller = new Repeller(width / 2, height/2);
+  att1 = new Attractor(235, height/2);
+  att2 = new Attractor(565, height/2);
 }
 
 function draw() {
-  background(255);
-  repeller.setPower(params.repellerpower);
-  repeller.setSpeed(params.rotatespeed);
-  repeller.move();
-
-  for (let i = 0; i < params.particlevalue; i++) {
-    emitter.addParticle();
-  }
-
-  let gravity = createVector(0, 0.1);
-  emitter.applyForce(gravity);
-  emitter.applyRepeller(repeller);
-  emitter.run();
-
-  repeller.show();
+background(255);
+repeller.move();
+emitter1.addParticle();
+emitter1.addParticle();
+emitter2.addParticle();
+emitter2.addParticle();
+let gravity1 = createVector(0, 0.005);
+emitter1.applyForce(gravity1);
+emitter1.applyRepeller(repeller);
+emitter1.applyAttractor(att1);
+emitter1.applyAttractor(att2);
+emitter1.run();
+let gravity2 = createVector(0, -0.005);
+emitter2.applyForce(gravity2);
+emitter2.applyRepeller(repeller);
+emitter2.applyAttractor(att1);
+emitter2.applyAttractor(att2);
+emitter2.run();
+repeller.show();
+// att1.show();
+// att2.show();
 }
