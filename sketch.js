8a6ecbdf88c121ac1d6a2822466dd250e0640d25
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-12-12 06:52:55 +0900

    스케치 수정
    gui 삭제 및 attractor 추가

--- sketch.js
+++ sketch.js
@@ -1,52 +1,60 @@
-let emitter;
-let repeller;
+// let gui;
 
-let gui;
-
-let params = {
-  rotatespeed: 0.01,
-  rotatespeedMin: -0.05,
-  rotatespeedMax: 0.05,
-  rotatespeedStep: 0.005,
-  particlevalue: 3,
-  particlevalueMin: 0,
-  particlevalueMax: 7,
-  particlevalueStep: 0.5,
-  repellerpower: 400,
-  repellerpowerMin: 0,
-  repellerpowerMax: 1000,
-  repellerpowerStep: 10
-};
+// let params = {
+//   rotatespeed: 0.01,
+//   rotatespeedMin: -0.05,
+//   rotatespeedMax: 0.05,
+//   rotatespeedStep: 0.005,
+//   particlevalue: 3,
+//   particlevalueMin: 0,
+//   particlevalueMax: 7,
+//   particlevalueStep: 0.5,
+//   repellerpower: 400,
+//   repellerpowerMin: 0,
+//   repellerpowerMax: 1000,
+//   repellerpowerStep: 10
+// };
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 800);
   getAudioContext().suspend();
 
-  emitter = new Emitter(width / 2, height / 2);
+  particle = new Particle(width / 2, 100);
   repeller = new Repeller(width / 2, height / 2);
+  att1 = new Attractor(235, height/2);
+  att2 = new Attractor(565, height/2);
+  // gui = createGui("test slider");
 
-  gui = createGui("test slider");
-
-  gui.addObject(params);
-  gui.setPosition(510, 10);
+  // gui.addObject(params);
+  // gui.setPosition(510, 10);
 }
 
 function draw() {
   background(255);
-  repeller.setPower(params.repellerpower);
-  repeller.setSpeed(params.rotatespeed);
   repeller.move();
-
-  for (let i = 0; i < params.particlevalue; i++) {
-    emitter.addParticle();
-  }
-
-  let gravity = createVector(0, 0.1);
-  emitter.applyForce(gravity);
-  emitter.applyRepeller(repeller);
-  emitter.run();
-
+  let gravity1 = createVector(0, 0.005);
+  particle.applyForce(gravity1);
+  particle.applyRepeller(repeller);
+  particle.applyAttractor(att1);
+  particle.applyAttractor(att2);
+  particle.run();
   repeller.show();
+  att1.show();
+  att2.show();
+  // repeller.setPower(params.repellerpower);
+  // repeller.setSpeed(params.rotatespeed);
+  // repeller.move();
+
+  // for (let i = 0; i < params.particlevalue; i++) {
+  //   emitter.addParticle();
+  // }
+
+  // let gravity = createVector(0, 0.1);
+  // emitter.applyForce(gravity);
+  // emitter.applyRepeller(repeller);
+  // emitter.run();
+
+  // repeller.show();
 }
 
 function mousePressed() {
